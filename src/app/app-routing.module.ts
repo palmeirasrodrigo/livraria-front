92c715316d15e23c21c1c0b19b2f524a8b13b075
palmeirasrodrigo <palmeirasrodrigo@hotmail.com>
2021-03-09 11:20:23 -0300

    funcionalidade deletar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
-import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
-import { HomeComponent } from './components/views/home/home.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CategoriaDeleteComponent } from "./components/view/categoria/categoria-delete/categoria-delete.component";
+import { CategoriaCreateComponent } from "./components/views/categoria/categoria-create/categoria-create.component";
+import { CategoriaReadComponent } from "./components/views/categoria/categoria-read/categoria-read.component";
+import { HomeComponent } from "./components/views/home/home.component";
 
 const routes: Routes = [
-  { 
-    path:'',
-    component: HomeComponent
+  {
+    path: "",
+    component: HomeComponent,
   },
   {
-    path:'categorias',
-    component: CategoriaReadComponent
+    path: "categorias",
+    component: CategoriaReadComponent,
   },
   {
-    path:'categorias/create',
-    component: CategoriaCreateComponent
+    path: "categorias/create",
+    component: CategoriaCreateComponent,
+  },
+  {
+    path: "categorias/delete/:id",
+    component: CategoriaDeleteComponent,
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
