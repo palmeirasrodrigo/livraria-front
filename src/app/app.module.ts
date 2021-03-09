9900c09d65f40b0de9b58629b203cb948c959191
palmeirasrodrigo <palmeirasrodrigo@hotmail.com>
2021-03-09 10:39:11 -0300

    finalizando o create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HeaderComponent } from "./components/template/header/header.component";
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import { FooterComponent } from './components/template/footer/footer.component';
-import { NavComponent } from './components/template/nav/nav.component';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatIconModule} from '@angular/material/icon';
-import {HomeComponent} from './components/views/home/home.component';
-import {MatCardModule} from '@angular/material/card';
-import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
-import {MatTableModule} from '@angular/material/table';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { FooterComponent } from "./components/template/footer/footer.component";
+import { NavComponent } from "./components/template/nav/nav.component";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { HomeComponent } from "./components/views/home/home.component";
+import { MatCardModule } from "@angular/material/card";
+import { CategoriaReadComponent } from "./components/views/categoria/categoria-read/categoria-read.component";
+import { MatTableModule } from "@angular/material/table";
+import { MatButtonModule } from "@angular/material/button";
+import { CategoriaCreateComponent } from "./components/views/categoria/categoria-create/categoria-create.component";
+import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
@@ -26,7 +31,8 @@ import {MatButtonModule} from '@angular/material/button';
     FooterComponent,
     NavComponent,
     HomeComponent,
-    CategoriaReadComponent
+    CategoriaReadComponent,
+    CategoriaCreateComponent,
   ],
   imports: [
     BrowserModule,
@@ -39,9 +45,13 @@ import {MatButtonModule} from '@angular/material/button';
     MatCardModule,
     MatTableModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/views/categoria/categoria-read/categoria-read.component.ts
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -1,28 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Categoria } from '../categoria.model';
-import { CategoriaService } from '../categoria.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Categoria } from "../categoria.model";
+import { CategoriaService } from "../categoria.service";
 
 @Component({
-  selector: 'app-categoria-read',
-  templateUrl: './categoria-read.component.html',
-  styleUrls: ['./categoria-read.component.css']
+  selector: "app-categoria-read",
+  templateUrl: "./categoria-read.component.html",
+  styleUrls: ["./categoria-read.component.css"],
 })
 export class CategoriaReadComponent implements OnInit {
+  categorias: Categoria[] = [];
 
-  categorias: Categoria[] = []
+  displayedColumns: string[] = ["id", "nome", "descricao", "livros", "acoes"];
 
-  displayedColumns: string[] = ['id', 'nome', 'descricao', 'livros', 'acoes'];
-
-  constructor(private service: CategoriaService) { }
+  constructor(private service: CategoriaService, private router: Router) {}
 
   ngOnInit(): void {
     this.findAll();
   }
 
   findAll() {
-    this.service.findAll().subscribe(resposta => {
+    this.service.findAll().subscribe((resposta) => {
       this.categorias = resposta;
-    })
+    });
   }
- 
+
+  navegarParaCategoriaCreate() {
+    this.router.navigate(["categorias/create"]);
+  }
 }
