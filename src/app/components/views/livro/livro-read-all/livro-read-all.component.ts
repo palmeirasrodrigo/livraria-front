ca33c03d76cc4089aadd0a809b53e07981e2fcf1
palmeirasrodrigo <palmeirasrodrigo@hotmail.com>
2021-03-09 22:29:34 -0300

    Criar livro

--- src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Livro } from "../livro.model";
 import { LivroService } from "../livro.service";
 
@@ -15,7 +15,11 @@ export class LivroReadAllComponent implements OnInit {
 
   livros: Livro[] = [];
 
-  constructor(private service: LivroService, private route: ActivatedRoute) {}
+  constructor(
+    private service: LivroService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.id_cat = this.route.snapshot.paramMap.get("id_cat")!;
@@ -26,5 +30,9 @@ export class LivroReadAllComponent implements OnInit {
     this.service.findAllByCategoria(this.id_cat).subscribe((resposta) => {
       this.livros = resposta;
     });
+  }
+
+  navegarParaCriarLivro(): void {
+    this.router.navigate([`categorias/${this.id_cat}/livros/create`]);
   }
 }
